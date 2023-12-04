b8934f43bca7d3c07882152cf3cfa519cdd40ebc
Amalcr7 <amaldevsputhur73@gmail.com>
2023-12-04 14:42:26 +0530

    testing1

---
+++ python1/hash.py
@@ -0,0 +1,12 @@
+side = int(input("Please Enter any Side of a Square  : "))
+i = 0
+print("Square hash Pattern")
+
+while(i < side):
+    j = 0
+    while(j < side):
+        j = j + 1
+        print('#', end = '  ')
+    i = i + 1
+    print('')
+
